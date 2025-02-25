b9776311ebe33a2e3467a3f3f7dc27f8646a83ba
Joao Pedro Fonseca Dantas <fonsdant@gmail.com>
2025-02-25 10:10:37 -0300

    adjust docusaurus config
    Signed-off-by: Joao Pedro Fonseca Dantas <fonsdant@gmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,23 +5,25 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: 'fonsdant',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://fonsdant.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'fonsdant', // Usually your GitHub org/user name.
+  projectName: 'fonsdant.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
